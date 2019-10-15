e94c46efb980ccdb7f274d1bee8efbd41f64bebd
mohtadi <mohtadib@spacebarmedia.com>
2019-10-15 15:19:10 +0100

    in progresss

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
+    res.send('hello world');
     
 res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
