22269130a41aec8b815e5b316862e45bec5a79c2
Mohtadi Bakali <teddybak@hotmail.com>
2019-10-20 08:25:49 +0100

    in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CvComponent} from './cv/cv.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cv", component:CvComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
